8f299113538754ffed235beb0a33370716aa4a37
Samuelson23 <3samuelmg@gmail.com>
2023-04-06 16:29:39 +0200

    exercise4

---
+++ javascript/ExercisesExtra/1-Padawan/exercise4.js
@@ -0,0 +1,17 @@
+/*
+Usa un bucle para sumar el total de las ventas (sellCount) de todos los productos.
+*/
+const products = [
+    { name: "Funko Dr. Strange", sellCount: 10 },
+    { name: "Mochila de protones: Ghostbusters", sellCount: 302 },
+    { name: "Sable laser FX", sellCount: 23 },
+    { name: "Varita de Voldemort", sellCount: 6 },
+  ];
+
+  let sumatorio=0;
+
+  products.forEach(element=>{
+    sumatorio+=element.sellCount
+  })
+
+  console.log(sumatorio)
