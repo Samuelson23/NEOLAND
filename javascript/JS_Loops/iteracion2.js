3d44ab101a86d481afc02b2e4b71d437c5bc74cc
Samuelson23 <3samuelmg@gmail.com>
2023-04-10 17:46:46 +0200

    iteracion2

--- javascript/JS_Loops/iteracion2.js
+++ javascript/JS_Loops/iteracion2.js
@@ -8,24 +8,28 @@ a true o false en consecuencia. Una vez lo tengas compruébalo con un ***console
 Puedes usar este array para probar tu función: */
 
 const alumns = [
-    {name: 'Pepe Viruela', T1: false, T2: false, T3: true}, 
-	{name: 'Lucia Aranda', T1: true, T2: false, T3: true},
-	{name: 'Juan Miranda', T1: false, T2: true, T3: true},
-	{name: 'Alfredo Blanco', T1: false, T2: false, T3: false},
-	{name: 'Raquel Benito', T1: true, T2: true, T3: true}
-]
+  { name: "Pepe Viruela", T1: false, T2: false, T3: true },
+  { name: "Lucia Aranda", T1: true, T2: false, T3: true },
+  { name: "Juan Miranda", T1: false, T2: true, T3: true },
+  { name: "Alfredo Blanco", T1: false, T2: false, T3: false },
+  { name: "Raquel Benito", T1: true, T2: true, T3: true },
+];
 
-alumns.forEach(alumno=>{
-    if(alumno.T1==true && alumno.T2==true){
-        alumno.isApproved=true
-    }else if(alumno.T1==true && alumno.T3==true){
-        alumno.isApproved=true
-    }else if(alumno.T2==true && alumno.T3==true){
-        alumno.isApproved=true
-    }
-})
+alumns.forEach((alumno) => {
+  alumno.isApproved = false;    //inicializo la propiedad isApproved para que todos los alumnos la tengan en false por defecto
 
-alumns.forEach(alum=>{
+  if (alumno.T1 == true && alumno.T2 == true) {
+    alumno.isApproved = true;
+  } else if (alumno.T1 == true && alumno.T3 == true) {
+    alumno.isApproved = true;
+  } else if (alumno.T2 == true && alumno.T3 == true) {
+    alumno.isApproved = true;
+  }
+});
+
+console.log(alumns);    //muestro por consola el array con la nueva propiedad
+
+alumns.forEach(alum=>{  //muestro por consola sólo los alumnos que han superado 2 trimestres
     if(alum.isApproved==true){
     console.log(`${alum.name} ha superado dos trimestres`)}
-})
+}) 
