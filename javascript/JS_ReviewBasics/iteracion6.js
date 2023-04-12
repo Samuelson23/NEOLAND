8e5a1c63ac0abaaea71658380918c12a94282f63
Samuelson23 <3samuelmg@gmail.com>
2023-04-12 12:36:03 +0200

    iteracion6

---
+++ javascript/JS_ReviewBasics/iteracion6.js
@@ -0,0 +1,26 @@
+/*
+**Iteración #6: Función swap**
+
+Crea una función llamada `swap()` que reciba un array y dos parametros que sean indices del array. 
+La función deberá intercambiar la posición de los valores de los indices que hayamos enviado como parametro. Retorna el array resultante.
+*/
+const fantasticFour = [
+    "La antorcha humana",
+    "Mr. Fantástico",
+    "La mujer invisible",
+    "La cosa",
+  ];
+  
+  console.log(fantasticFour)
+  const swap = (array,posX,posY) => {
+    let posAux="";
+    posAux=array[posX]
+    array[posX]=array[posY]
+    array[posY]=posAux
+  
+    return array
+  
+  }
+  
+  swap(fantasticFour,1,3)
+  console.log(fantasticFour)
