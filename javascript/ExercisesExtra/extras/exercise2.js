bf64ad8f349e0c76d858b4ab543f81b343584f2b
Samuelson23 <3samuelmg@gmail.com>
2023-04-13 14:11:41 +0200

    exercise2

---
+++ javascript/ExercisesExtra/extras/exercise2.js
@@ -0,0 +1,22 @@
+/*
+En base al siguiente javascript, modifícalo para crea una función para que podamos enviarle como parametro el precio y 
+la función nos calcule y retorne el precio con iva + los gastos de envio (4.95). 
+De esta forma estaremos optimizando el codigo y reutilizandolo.
+*/
+const price1 = 8;
+const ivaPrice1 = price1 * 1.21;
+const totalPrice1 = ivaPrice1 + 4.95;
+
+const price2 = 20;
+const ivaPrice2 = price2 * 1.21;
+const totalPrice2 = ivaPrice2 + 4.95;
+
+const calculaPrecio = (precio) => {
+    let precioTotal=precio*1.21 + 4.95
+    console.log(precioTotal)
+}
+
+calculaPrecio(5)
+calculaPrecio(10)
+calculaPrecio(1)
+calculaPrecio(0)
