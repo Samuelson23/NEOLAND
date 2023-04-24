af58c90177baf33ccf80277b743d11d2c620cebe
Samuelson23 <3samuelmg@gmail.com>
2023-04-24 20:54:27 +0200

    iteracion1 -> interaccion con el DOM

--- javascript/JS_Dom/iteracion1_interaccion-con-el-dom/app.js
+++ javascript/JS_Dom/iteracion1_interaccion-con-el-dom/app.js
@@ -1,11 +1,3 @@
-/*
-1.5 Usa querySelector para mostrar por consola todos los elementos con el atributo 
-data-function="testMe".
-
-1.6 Usa querySelector para mostrar por consola el 3 personaje con el atributo 
-data-function="testMe".
-*/
-
 // 1.1 Usa querySelector para mostrar por consola el botón con la clase .showme
 const enseñame = document.querySelector(".showme");
 console.log(enseñame);
@@ -21,3 +13,15 @@ console.log(parrafos);
 //1.4 Usa querySelector para mostrar por consola todos los elementos con la clase.pokemon
 const pokemons = document.querySelectorAll(".pokemon");
 console.log(pokemons);
+
+//1.5 Usa querySelector para mostrar por consola todos los elementos con el atributo
+//data-function="testMe".
+const atrTestMe = document.querySelectorAll("[data-function='testMe']");
+for (const elem of atrTestMe) {
+  console.log(elem);
+}
+console.log(atrTestMe);
+
+//1.6 Usa querySelector para mostrar por consola el 3 personaje con el atributo
+//data-function="testMe".
+console.log(atrTestMe[2]);
