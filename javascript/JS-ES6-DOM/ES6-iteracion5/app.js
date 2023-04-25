ab7549664d77bddf916a76c84f4b8d993f1e7bd9
Samuelson23 <3samuelmg@gmail.com>
2023-04-25 20:12:58 +0200

    ejercicios de ES6 que requerian DOM

---
+++ javascript/JS-ES6-DOM/ES6-iteracion5/app.js
@@ -0,0 +1,27 @@
+/* 5.6 Dado el siguiente html y javascript, utiliza .filter() para mostrar por consola 
+los streamers que incluyan la palabra introducida en el input. De esta forma, si 
+introduzco 'Ru' me deberia de mostrar solo el streamer 'Rubius'. Si
+introduzco 'i', me deberia de mostrar el streamer 'Rubius' e 'Ibai'.*/
+
+const streamers = [
+  { name: "Rubius", age: 32, gameMorePlayed: "Minecraft" },
+  { name: "Ibai", age: 25, gameMorePlayed: "League of Legends" },
+  { name: "Reven", age: 43, gameMorePlayed: "League of Legends" },
+  { name: "AuronPlay", age: 33, gameMorePlayed: "Among Us" },
+];
+
+const input = document.querySelector("[data-function='toFilterStreamers']"); //recogemos el input de html
+let letra = "";
+//creamos un eventListener  para ver que se está introduciendo en el input
+input.addEventListener("click", (event) => {
+  //cada vez que hagamos click limpiamos la consola y mostramos el resultado
+  console.clear();
+  //filtramos el array de streamers buscando la coincidencia
+  const filtrar = streamers.filter((elem) => {
+    //convertimos tanto el nombre del streamer como el valor introducido en minusculas
+    nameStr = elem.name.toLowerCase();
+    letra = event.target.value;
+    //condicion de si el nombre incluye lo introducido
+    nameStr.includes(letra.toLowerCase()) && console.log(elem.name);
+  });
+});
