8bf6f28aa8dd54020629fd35cfb3b677db7988b9
Samuelson23 <3samuelmg@gmail.com>
2023-04-27 17:04:42 +0200

    iteracion async-await

---
+++ javascript/JS_Asincronia/asinc-await/iteracion1/app.js
@@ -0,0 +1,17 @@
+/*
+2.1 Gestiona la siguiente promesa para esperar a ejecutar el console usando 
+async-await.
+*/
+const runTimeOut = async () => {
+  const promise = await new Promise((resolve) => {
+    setTimeout(function () {
+      resolve();
+    }, 2000);
+  });
+
+  promise.then(() => {
+    console.log("Time out!");
+  });
+};
+
+runTimeOut();
---
+++ javascript/JS_Asincronia/asinc-await/iteracion2/app.js
@@ -0,0 +1,11 @@
+/*
+2.2 Convierte la siguiente función con un fetch utilizando async-await. 
+Recuerda que para usar .fetch() tendrás que probar el ejercicio en el navegador;
+*/
+async function getCharacters() {
+  await fetch("https://rickandmortyapi.com/api/character")
+    .then((res) => res.json())
+    .then((characters) => console.log(characters));
+}
+
+getCharacters();
