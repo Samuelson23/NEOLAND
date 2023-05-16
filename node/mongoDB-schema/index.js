cc6b779285a68c3496de24111c9c4cad5d1d4b31
Samuelson23 <3samuelmg@gmail.com>
2023-05-16 20:48:47 +0200

    arquitectura mongoDB schema y models

--- node/mongoDB-schema/index.js
+++ node/mongoDB-schema/index.js
@@ -1,3 +1,15 @@
+// Creamos el servidor web y lo conectamos a la DB mediante el puerto 8080
+
 const { connect } = require("./api/utils/db");
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT;
+const server = express();
+
+server.listen(PORT, () => {
+  console.log(`Escuchando en el puerto ${PORT}`);
+});
 
 connect();
