7b3eddd38d5a4a98a448da7aaa7c053a8d11d61c
Samuelson23 <3samuelmg@gmail.com>
2023-05-16 21:25:03 +0200

    web scrapping

--- node/web-scrapping/index.js
+++ node/web-scrapping/index.js
@@ -1,63 +1,37 @@
-//importamos las LIBRERIAS fs (para escribir texto plano) y puppeteer (para hacer web scrapping)
 const fs = require("fs");
 const puppeteer = require("puppeteer");
 
-//creamos una variable para la url base. Puede ir en mayusculas porque no va a cambiar nunca
-const BASE_URL = "https://www.sinhumo.net/s-1/sabores_de_liquidos-bebidas";
+const BASE_URL = "https://www.sprintersports.com/tienda-vans";
 
-//creamos la funcion que hará web scrapping. Al ser una función que se ejecuta en internet DEBE DE SER asincrona
 const scrapping = async () => {
-  //creamos el navegador para que la funcion lo abra en una ventana al inicializar
-
   const browser = await puppeteer.launch({
-    headless: false, //---> para que lo abra maximizado
-    defaultViewport: null, //---> para que se mantenga maximizado
+    headless: false,
+    defaultViewport: null,
     args: ["--start-maximized"],
   });
+  const page = await browser.newPage();
+  await page.goto(BASE_URL);
+  await page.waitForTimeout(3000);
 
-  const page = await browser.newPage(); //abrimos el navegador
-  await page.goto(BASE_URL); //metemos la URL al navegador mediante el metodo "goto"
-  await page.waitForTimeout(30000); //le decimos al navegador que se espere 4seg para cargar bien toda la info
+  // aqui iria el clickar a la siguiente pag o que haga scroll
 
-  //ahora habria que hacer scroll o clickar a la siguiente pagina
-  //pero no lo vamos a hacer xD
+  await page.waitForSelector(".row");
 
-  //con "waitForSelector" le decimos que espere a que los elementos esten pintados
-  await page.waitForSelector("#wrapper");
-
-  // hay que hacer click en esta clase que es la del boton del ser mayor de edad --- class=cn_element cn_html id-d3ed81c8-2a1c-4650-be6f-26c4451467a5
-
-  //guardamos los datos cuando esten disponibles
-  const tittle = await page.$$eval("h3 product-title", (nodes) =>
-    //$$eval --> evalua que exista esa clase y la callback evalua todos los nodos
-    //recorremos todos los nodos y los mapeamos para quedarnos con los datos que nos interesan
-    //en la variable "tittle" nos va a guardar todos los innerText de los elementos que encuentre
-    nodes.map((n) => n.innerText)
-  );
-  const image = await page.$$eval("product-img", (nodes) =>
-    nodes.map((n) => n.src)
-  );
-  const price = await page.$$eval("price", (nodes) =>
-    nodes.map((n) => n.innerText)
-  );
-  console.log(tittle);
-  //creamos un objeto para almacenar todos los datos recogidos
-
-  const productos = tittle.map((item, index) => ({
-    name: tittle[index],
-    img: image[index],
-    price: price[index],
-  }));
-
-  //para borrar el ultimo elemento ya que al mapearlo sale un elemento vacio
-  productos.pop();
-  console.log(productos);
-  const stProducts = JSON.stringify(productos);
-  fs.writeFile("liquidosBebidas.json", stProducts, () => {
-    console.log("copiado");
+  const tittle = await page.$$eval(".product__name", (nodes) => {
+    nodes.map((n) => n.innerText);
   });
+  const image = await page.$$eval(
+    ".v-lazy-image v-lazy-image-loaded",
+    (nodes) => {
+      nodes.map((n) => n.innerText);
+    }
+  );
 
-  await browser.close();
+  const products = tittle.map((item, index) => {
+    details: tittle[index];
+    img: image[index];
+  });
+  console.log(products);
 };
 
 scrapping();
---
+++ node/web-scrapping/indexCursos.js
@@ -0,0 +1,68 @@
+//importamos las LIBRERIAS fs (para escribir texto plano) y puppeteer (para hacer web scrapping)
+const fs = require("fs");
+const puppeteer = require("puppeteer");
+
+//creamos una variable para la url base. Puede ir en mayusculas porque no va a cambiar nunca
+const BASE_URL = "https://www.pildorasinformaticas.es/all-courses/";
+
+//creamos la funcion que hará web scrapping. Al ser una función que se ejecuta en internet DEBE DE SER asincrona
+const scrapping = async () => {
+  //creamos el navegador para que la funcion lo abra en una ventana al inicializar
+
+  const browser = await puppeteer.launch({
+    headless: false, //---> para que lo abra maximizado
+    defaultViewport: null, //---> para que se mantenga maximizado
+    args: ["--start-maximized"],
+  });
+
+  const page = await browser.newPage(); //abrimos el navegador
+  await page.goto(BASE_URL); //metemos la URL al navegador mediante el metodo "goto"
+  await page.waitForTimeout(4000); //le decimos al navegador que se espere 4seg para cargar bien toda la info
+
+  //ahora habria que hacer scroll o clickar a la siguiente pagina
+  //pero no lo vamos a hacer xD
+
+  //con "waitForSelector" le decimos que espere a que los elementos esten pintados
+  await page.waitForSelector(".container");
+
+  // hay que hacer click en esta clase que es la del boton del ser mayor de edad --- class=cn_element cn_html id-d3ed81c8-2a1c-4650-be6f-26c4451467a5
+
+  //guardamos los datos cuando esten disponibles
+  const tittle = await page.$$eval(".item-title", (nodes) =>
+    //$$eval --> evalua que exista esa clase y la callback evalua todos los nodos
+    //recorremos todos los nodos y los mapeamos para quedarnos con los datos que nos interesan
+    //en la variable "tittle" nos va a guardar todos los innerText de los elementos que encuentre
+    nodes.map((n) => n.innerText)
+  );
+  const image = await page.$$eval(
+    ".attachment-full size-full wp-post-image",
+    (nodes) => nodes.map((n) => n.srcset)
+  );
+  const price = await page.$$eval(".item-credits", (nodes) =>
+    nodes.map((n) => n.innerText)
+  );
+  const description = await page.$$eval(".item-desc", (nodes) => {
+    nodes.map((n) => n.innerText);
+  });
+  console.log(tittle);
+  //creamos un objeto para almacenar todos los datos recogidos
+
+  const productos = tittle.map((item, index) => ({
+    name: tittle[index],
+    img: image[index],
+    description: description[index],
+    price: price[index],
+  }));
+
+  //para borrar el ultimo elemento ya que al mapearlo sale un elemento vacio
+  productos.pop();
+  console.log(productos);
+  const stProducts = JSON.stringify(productos);
+  fs.writeFile("cursos.json", stProducts, () => {
+    console.log("copiado");
+  });
+
+  await browser.close();
+};
+
+scrapping();
