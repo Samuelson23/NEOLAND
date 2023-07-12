6e9bb6c9d9d4b6a03f5e032c110095f96fccfa53
Samuelson23 <3samuelmg@gmail.com>
2023-07-12 16:13:35 +0200

    typescript functions

---
+++ Typescript/functions01.ts
@@ -0,0 +1,29 @@
+/*
+Crear una funion que reciba un parametro. Primero le ponemos tipo string para comprobar que no se puede llamar a la funcion enviando
+un number. Despues hacemos una union de tipos para poder pasarle tanto string como number
+*/
+
+function displayAlert(message:string|number) {
+    alert('The message is ' + message);
+}
+
+displayAlert(1)
+
+/*
+La función sum suma los números de una matriz y devuelve el resultado.
+Establezca el tipo del parámetro input en una matriz de valores number, luego, el tipo de valor devuelto de la función en number 
+y establezca el tipo de la variable total en number.
+*/
+
+function sum(input:number[]): number {
+    let total:number =  0;
+    for(let count = 0; count < input.length; count++) {
+        if(isNaN(input[count])){
+            continue;
+        }
+        total += Number(input[count]);
+    }
+    return total;
+}
+
+console.log(sum([5,6]))
